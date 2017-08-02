216d9c3217247654bc38a078ee673eba394484e4
Tilman Lindig <soccer_14_rhs@live.ca>
2017-08-01 22:30:42 -0400

    Intro Screen and Buttons working

--- game.js
+++ game.js
@@ -1,11 +1,19 @@
 
 var currentDescription;
 var shapeArray = [];
+var inSet = false;
+var points = 0;
 
 function startGame() {
+    $("#title").hide();
+    $("#startButton").hide();
+    $("#leftButton").show();
+    $("#rightButton").show();
     myGameArea.start();
     shapeArray.push(new Rect(30,30,"red", 10, 120));
-    shapeArray.push(new Circle(15,"green",60,120));
+    shapeArray.push(new Circle(15,"green",10,120));
+    shapeArray.push(new Triangle(30,30, "blue", 10,120));
+    shapeArray.push(getShape(3, 30, 3, 10, 120));
 }
 
 var myGameArea = {
@@ -15,18 +23,18 @@ var myGameArea = {
         this.canvas.height = $(document).height()-30;
         this.context = this.canvas.getContext("2d");
         $("body").prepend(this.canvas);
-        this.interval = setInterval(updateGameArea, 10);
+        this.interval = setInterval(updateGameArea, 1);
         $(document).keydown(function(e) {
             switch(e.which) {
                 case 37: // left
-                alert('left');
+                analyseResult(1);
                 break;
 
                 case 38: // up
                 break;
 
                 case 39: // right
-                alert('right');
+                analyseResult(0);
                 break;
 
                 case 40: // down
@@ -39,6 +47,10 @@ var myGameArea = {
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    reset: function() {
+        shapeArray.push(new Rect(30,30,"red", 10, 120));
+        shapeArray.push(new Circle(15,"green",60,120));
     }
 }
 
@@ -46,36 +58,18 @@ var myGameArea = {
 function updateGameArea(){
     myGameArea.clear();
     for (var i = 0; i < shapeArray.length; i++) {
-      shapeArray[i].x += Math.random() > 0.5 ? -1 : 1;
+      //shapeArray[i].x += Math.random() > 0.5 ? -1 : 1;
       shapeArray[i].update();
     };
 }
 
-function Rect(width, height, colour, x, y) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.update = function(){
-      ctx = myGameArea.context;
-      ctx.fillStyle = colour;
-      ctx.fillRect(this.x, this.y, this.width, this.height);
+function analyseResult(yes){
+    if(yes == inSet){
+      points += 50;
+    }
+    else{
+      points -= 10;
     }
 }
 
-function Circle(radius, colour, x, y){
-    this.radius = radius;
-    this.x = x;
-    this.y = y;
-    this.update = function(){
-      var context = myGameArea.context;
-      context.beginPath();
-      context.arc(this.x, this.y, radius, 0, 2 * Math.PI, false);
-      context.fillStyle = colour;
-      context.fill();
-      context.lineWidth = 5;
-      context.strokeStyle = '#003300';
-      //context.stroke();
-    }
-}
 
